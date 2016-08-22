fea6a8b7e6665ad404dd3c94e58804aa81d3b02e
Prasad guda <gudadurga.p@hcl.com>
2016-08-22 19:26:07 +0000

    second commit.

--- src/main/java/com/sudheendra/app/App.java
+++ src/main/java/com/sudheendra/app/App.java
@@ -11,6 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!, new world and a wonder" );
         System.out.println( "Hello World!, new world" );
-        System.out.println("Dabidi");
+        System.out.println("Dabidi2");
     }
 }
