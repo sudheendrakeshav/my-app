d3be61c5d4c5dc772e5216a33162640ddb391583
sudheendra <email2sudheendra1@gmail.com>
2016-08-23 04:43:55 -0700

    gradle keyword in the code

--- src/main/java/com/sudheendra/app/App.java
+++ src/main/java/com/sudheendra/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!, new world and a wonder" );
+        System.out.println( "Hello World!, new world and a wonder and the next most thing is gradle" );
     }
 }
